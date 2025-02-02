1856e37dbf741b2e9472ce006107aa914bff8a39
Muhammad Sameer <muhammadsameer.css@gmail.com>
2025-02-02 18:15:11 +0500

    update static file changes

--- config/settings.py
+++ config/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 #  Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
-# Add this if you have a specific directory for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# # Add this if you have a specific directory for static files
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 
